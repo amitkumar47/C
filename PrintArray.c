99d6c4f663c09de72f029267aa69de68db5e3174
amitkumar47 <amits4787@gamil.com>
2018-10-02 15:01:25 +0530

    print the arrat

---
+++ PrintArray.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,array[8]={1,2,3,4,5,7,8,9};
+    for (a=0 ; a<8 ; a++)
+    {
+        printf("%d\n",array[a]);
+    }
+    return 0;
+}
